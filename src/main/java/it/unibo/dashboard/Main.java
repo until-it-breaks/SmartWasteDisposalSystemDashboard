937913803bf458e3c90811095622f754564ccc14
Jiekai Sun <jiekai.sun@studio.unibo.it>
2024-11-13 13:13:09 +0100

    Removed port testing

--- src/main/java/it/unibo/dashboard/Main.java
+++ src/main/java/it/unibo/dashboard/Main.java
@@ -3,8 +3,6 @@ package it.unibo.dashboard;
 import it.unibo.dashboard.api.CommChannel;
 import it.unibo.dashboard.api.Dashboard;
 import jssc.SerialPort;
-import jssc.SerialPortEvent;
-import jssc.SerialPortEventListener;
 import jssc.SerialPortException;
 import jssc.SerialPortList;
 
@@ -13,88 +11,22 @@ import jssc.SerialPortList;
  * There is also the option to provide the port via args with either {./gradlew run --args="portname"} or {java -jar ./dashboard-all.jar "portname"}.
  */
 public class Main {
-    private static volatile boolean portFound = false;
-    private static final int TIMEOUT_DELTA = 5000;
-
     public static void main(final String[] args) {
         if (args.length == 0) {
             final String[] ports = SerialPortList.getPortNames();
             if (ports.length == 0) {
-                System.out.println("No active ports");
+                System.out.println("No ports found");
             } else {
-                for (final String port : ports) {
-                    if (portFound) {
-                        break;
-                    }
-                    System.out.println("Testing port: " + port);
-                    new Thread(() -> {
-                        try {
-                            tryPort(port);
-                        } catch (SerialPortException e) {
-                            e.printStackTrace();
-                        }
-                    }).start();
+                System.out.println("Run the app with one of the following argument with the command {java -jar ./dashboard-all.jar portname} or {./gradlew run --args=\"portname\"}");
+                for (String port : ports) {
+                    System.out.println(port);
                 }
             }
         } else {
-            try {
-                tryPort(args[0]);
-            } catch (SerialPortException e) {
-                e.printStackTrace();
-            }
+            startApplication(args[0]);
         }
     }
 
-    private static void tryPort(final String portName) throws SerialPortException {
-        final StringBuilder receivedData = new StringBuilder("");
-        final SerialPort serialPort = new SerialPort(portName);
-        serialPort.openPort();
-        serialPort.setParams(SerialPort.BAUDRATE_9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
-        serialPort.setFlowControlMode(SerialPort.FLOWCONTROL_RTSCTS_IN | SerialPort.FLOWCONTROL_RTSCTS_OUT);
-    
-        final long timeout = System.currentTimeMillis() + TIMEOUT_DELTA;
-    
-        serialPort.addEventListener(new SerialPortEventListener() {
-            @Override
-            public void serialEvent(final SerialPortEvent event) {
-                if (event.isRXCHAR() && !portFound) {
-                    try {
-                        String received = serialPort.readString(event.getEventValue());
-                        received = received.trim();
-                        if (received != null) {
-                            receivedData.append(received);
-                            if (receivedData.toString().equals("ArduinoUno")) {
-                                System.out.println("Arduino detected on " + portName);
-                                portFound = true;
-                                serialPort.closePort();
-                                startApplication(portName);
-                            }
-                        }
-                    } catch (final SerialPortException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-    
-        while (System.currentTimeMillis() <= timeout) {
-            if (portFound) {
-                if (serialPort.isOpened()) {
-                    serialPort.closePort();
-                }
-                return;
-            }
-            try {
-                Thread.sleep(100);
-            } catch (final InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        System.out.println("No response from " + portName);
-        if (serialPort.isOpened()) {
-            serialPort.closePort();
-        }
-    }
 
     private static void startApplication(final String portName) {
         final DashBoardController controller = new DashBoardController();
